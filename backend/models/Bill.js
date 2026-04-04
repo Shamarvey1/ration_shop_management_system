d124de49854d44e689da944a1f95b4ac561d6c13
Shamarvey1 <ajitlookforbest@gmail.com>
2026-04-04 23:52:13 +0530

    Bill.js

---
+++ backend/models/Bill.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const billSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+    },
+
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+
+    paidAmount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    remainingAmount: {
+      type: Number,
+      required: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Bill = mongoose.model("Bill", billSchema);
+
+module.exports = Bill;
