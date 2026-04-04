5634d7fe3540ac5df70cc96635bc38862922f71c
Shamarvey1 <ajitlookforbest@gmail.com>
2026-04-05 02:00:12 +0530

    routes/billRoutes.js

---
+++ backend/routes/billRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { createBill, getBills } = require("../controllers/billController");
+const { protect } = require("../middleware/authMiddleware");
+
+
+router.post("/", protect, createBill);
+router.get("/", protect, getBills);
+
+module.exports = router;
